2ebfeca5e61b96aff2b402f29dda2db6ffa01279
Diesel <dnspronin@gmail.com>
2024-09-03 19:32:36 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,7 @@
+allHomework = 12
+all_time = 1.5
+name = ("Python")
+
+time_for_task = all_time / allHomework
+
+print("Курс:", name, "всего задач:", allHomework, "затрачено часов:", all_time, "среднее время выполнения:", time_for_task)
