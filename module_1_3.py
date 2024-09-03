509e004f6e5c44eaf86193c3683a75c2faa77746
Diesel <dnspronin@gmail.com>
2024-09-03 19:49:27 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,13 @@
+from operator import truediv
+
+name = 'Denis'
+print('Name:', name)
+
+age = 29
+print('age:', age)
+
+age = 30
+print('New Age:', age)
+
+is_student = True
+print('Is Student:', is_student)
