1096aab49651f5fe110eec156f05ac7ba6c5fae9
Diesel <dnspronin@gmail.com>
2024-09-07 17:48:01 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+my_dict = {'Den': 1995, 'Max': 2002}
+print(my_dict)
+print(my_dict['Den'])
+my_dict.update({'Tom': 1992,
+                'Tim': 1999})
+del my_dict['Max']
+print(my_dict.get('Max'))
+print(my_dict)
+
+
+my_set = {1, 2, 3, 1, 1, 'Tom', 5, 4, 4}
+print(my_set)
+my_set.update({6, 9})
+my_set.discard(2)
+print(my_set)
